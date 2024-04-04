2290d4a9cf02324074c78dddf01207fd3ce748e7
Ana Livia Oliveira <143213393+livia0oliveira@users.noreply.github.com>
2024-04-04 09:36:04 -0300

    Add files via upload

---
+++ atividades/atividade-03/script.py
@@ -0,0 +1,47 @@
+import sqlite3
+import webbrowser
+
+conn = sqlite3.connect('project_DB.db')
+
+latitude_result = ""
+longitude_result = ""
+
+while True:
+    consulta = conn.execute("SELECT nome FROM TB_MUNICIPIOS")
+
+    x = 0
+
+    for row in consulta:
+        lista = row[0]
+        x += 1
+        print(x, ")", lista)
+        
+    id_muni = input("Qual município você deseja ver a localização?: ")
+    
+    if int(id_muni) > x:
+        print("Você não pode colocar um número maior do que está disponível")
+        print("")
+        continue
+
+    lati = conn.execute("SELECT latitude from TB_MUNICIPIOS WHERE id=" + id_muni)
+
+    for row in lati:
+        latitude = row[0]
+        
+    longi = conn.execute("SELECT longitude from TB_MUNICIPIOS WHERE id=" + id_muni)
+
+    for row in longi:
+        longitude = row[0]
+        
+    if (latitude == "") or (longitude == ""):
+        print("Esse município ainda não foi completado")
+        print("")
+        continue
+    else:
+        latitude_result = latitude
+        longitude_result = longitude
+        break
+
+url = "https://epsg.io/map#srs=4326&x=" + latitude_result + "&y=" + longitude_result + "&z=15&layer=streets"
+
+webbrowser.open(url, new=0)
